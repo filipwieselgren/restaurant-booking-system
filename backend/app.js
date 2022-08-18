52956e459e94fb46b23c55bd69b0278f5cdec998
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-08-18 12:50:10 +0200

    Port setup

--- backend/app.js
+++ backend/app.js
@@ -4,4 +4,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("./database.js");
 
+app.use(express.json());
+
 const BookingsModel = require("./models/Bookings.js");
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
