449f173d8226b3efb226b712f5fb77888ebfd98e
Tuva-Li Wass Vallin <tuvali.wahlin@icloud.com>
2022-08-18 10:42:15 +0200

    changed schema

--- backend/models/Bookings.js
+++ backend/models/Bookings.js
@@ -4,7 +4,11 @@ const Schema = mongoose.Schema;
 
 const bokingsSchema = new Schema({
   name: { type: String, required: true },
-  quantity: { type: Number, required: true },
+  amountOfPeople: { type: Number, required: true },
+  date: { type: String, required: true },
+  time: { type: Number, required: true },
+  email: { type: String, required: true },
+  phone: Number,
 });
 
 const BookingsModel = mongoose.model("Bookings", bokingsSchema);
