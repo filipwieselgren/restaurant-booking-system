2fe1b71fee6efbba0b87fdf2a885f3f28200105e
Sofia Borg <fia.borg@hotmail.com>
2022-08-21 19:23:02 +0100

    databse key individual

--- backend/database.js
+++ backend/database.js
@@ -3,11 +3,7 @@ require("dotenv").config();
 
 try {
   mongoose.connect(
-<<<<<<< HEAD
-    "mongodb://localhost/restaurant-booking-system",
-=======
-    "mongodb://localhost:27017/restaurant-booking-system",
->>>>>>> c1b597d8c96cb9b4d1ee47f19f91e3479eb2b481
+    process.env.MONGODBKEY,
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => console.log(" Mongoose is connected")
   );
