81cfe1002b08c2d977ffb38faf31942ae86979b0
Sofia Borg <fia.borg@hotmail.com>
2022-08-23 09:52:58 +0100

    added schema for contact and route for contact

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ require("mongoose");
 
 const booktableRoutes = require("./routes/booktableRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+const contactRoutes = require("./routes/contactRoutes");
 
 const express = require("express");
 const app = express();
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
 
 app.use("/booktable/", booktableRoutes);
 app.use("/admin", adminRoutes);
+app.use("/contact", contactRoutes);
 
 const port = 8080;
 app.listen(port, () => {
