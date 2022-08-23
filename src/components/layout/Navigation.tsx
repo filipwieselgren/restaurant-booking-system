d3234b8920c9dcd3e4916d3db6ed3e25a6781721
Sofia Borg <fia.borg@hotmail.com>
2022-08-23 10:40:45 +0100

    added basic styling to home

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../../styles/navigation.scss";
 
 export const Navigation = () => {
   return (
     <div className="navWrapper">
-      <div>
-        <div className="logo">LOGGA</div>
+      <div className="logoWrapper">
+        <Link to={"/"}>
+          <div className="logo">LOGGA</div>
+        </Link>
       </div>
-      <div>
-        <div className="menuBtn">MENY</div>
-        <div className="contactBtn">KONTAKT</div>
+      <div className="menuWrapper">
+        <Link to={"/meny"}>
+          <div className="menuBtn">MENY</div>
+        </Link>
+        <Link to={"/kontakt"}>
+          <div className="contactBtn">KONTAKT</div>
+        </Link>
       </div>
     </div>
   );
