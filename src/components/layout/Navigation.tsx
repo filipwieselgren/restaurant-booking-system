40a7503bc8b652ed6a37f80e7ee9c4fb93cc332e
Sofia Borg <fia.borg@hotmail.com>
2022-09-08 09:18:05 +0100

    added fake log in btn working

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,15 +1,36 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import "../../styles/layout/navigation.scss";
 const logoImg = require("../../assets/logo.png");
 
 export const Navigation = () => {
+  const url = useLocation();
+  const [adminText, setAdminText] = useState<Boolean>(false);
+
+  useEffect(() => {
+    if (url.pathname === "/") {
+      setAdminText(false);
+    } else {
+    }
+  }, [url.pathname]);
+
+  const hideText = () => {
+    setAdminText(true);
+  };
+
   return (
     <div className="navWrapper">
       <div className="loginWrapper">
-        {" "}
-        <Link to={"/admin/login"}>
-          <div className="loginBtn">Admin? Log in here!</div>
-        </Link>
+        {adminText ? (
+          <></>
+        ) : (
+          <Link to={"/admin/login"}>
+            <div className="loginBtn" onClick={hideText}>
+              Admin? Log in here!
+            </div>
+          </Link>
+        )}
       </div>
       <div className="logonmenu">
         <div className="logoWrapper">
